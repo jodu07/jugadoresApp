cac23b5e658934a71b87ff1e34bde2148a9feb50
jodu07 <jhonnat.dussan@gmail.com>
2022-05-02 17:42:59 -0500

    tablePlayer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment';
 import { FormsModule } from '@angular/forms';
 
+
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/components/player-table/player-table.component.ts
+++ src/app/components/player-table/player-table.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { Player } from '../../interfaces/player.interface';
+import { PlayerServiceService } from '../../services/player-service.service';
+import { TeamServiceService } from '../../services/team-service.service';
 
 @Component({
   selector: 'app-player-table',
@@ -7,9 +12,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayerTableComponent implements OnInit {
 
-  constructor() { }
+  public players$!: Observable<Player[]>;
+  public selectedPlayer!: Player;
+  public showModal = false;
+
+  constructor(private playerService: PlayerServiceService, private teamService: TeamServiceService) { }
 
   ngOnInit(): void {
+    this.players$ = this.playerService.getPlayers();
   }
 
+  newPlayer(){
+    this.showModal = true;
+   // this.selectedPlayer = [];
+    setTimeout(() => {
+      window.location.replace('#open-modal');
+    });
+  }
+
+  editPlayer(player: Player){
+    this.selectedPlayer = { ...player };
+    this.showModal = true;
+    setTimeout(() => {
+      window.location.replace('#open-modal');
+
+    }); 
+  }
+
+  deletePlayer(player: Player){
+    this.teamService
+    .getTeams()
+    .pipe(take(1))
+    .subscribe((teams:any) => {
+      const moddifieldPlayers = teams[0].players ? teams[0].players.filter((p: any) => p.key !== player.$key) : teams[0].players;
+      const formattedTeam = {
+        ...teams[0],
+        players: [...moddifieldPlayers]
+      };
+     // this.playerService.deletePlayer(player.$key);
+      this.teamService.editTeam(formattedTeam);      
+    });
+  }
+
+
+  closeDialog(){
+    this.showModal = false;
+  }
 }
+
