ff0fe0c99cc7fc31feb69d7c4c48a21a3608c0a7
jodu07 <jhonnat.dussan@gmail.com>
2022-05-02 17:11:23 -0500

    chanmges ok playerService

--- src/app/components/player-table/player-table.component.ts
+++ src/app/components/player-table/player-table.component.ts
@@ -1,42 +1,15 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
-import { Player } from 'src/app/interfaces/player.interface';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-player-table',
   templateUrl: './player-table.component.html',
   styleUrls: ['./player-table.component.sass']
 })
-export class PlayerTableComponent{
-  private playersDb: AngularFireList<Player>;
+export class PlayerTableComponent implements OnInit {
 
-  constructor(private db: AngularFireDatabase) {
-    this.playersDb = this.db.list('/players', ref => ref.orderByChild('name'));
+  constructor() { }
+
+  ngOnInit(): void {
   }
-
-  getPlayers(): Observable<Player[]>{
-     return this.playersDb.snapshotChanges().pipe(
-       map(changes => {
-         return changes.map(c => ({ $key: c.payload.key, ...c.payload.val() }))
-       })
-     )
-   }
-
-   addPlayer(player: Player){
-     return this.playersDb.push(player)
-   }
-
-
-   deletePlayer(id: string){
-     this.db.list('/players').remove(id);
-   }
-
-   editPlayer(newPlayerData){
-     const $key = newPlayerData.$key;
-     delete(newPlayerData.$key);
-     this.db.list('/players').update($key, newPlayerData);
-   }
-
 
 }
--- src/app/services/player-service.service.ts
+++ src/app/services/player-service.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Player } from '../interfaces/player.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerServiceService {
+  private playersDb: AngularFireList<Player>;
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) {
+    this.playersDb = this.db.list('/players', ref => ref.orderByChild('name'));
+  }
+
+  getPlayers(): Observable<Player[]>{
+     return this.playersDb.snapshotChanges().pipe(
+       map((changes:any) => {
+         return changes.map((c: { payload: { key: any; val: () => any; }; }) => ({ $key: c.payload.key, ...c.payload.val() }))
+       })
+     )
+   }
+
+   addPlayer(player: Player){
+     return this.playersDb.push(player)
+   }
+
+
+   deletePlayer(id: string){
+     this.db.list('/players').remove(id);
+   }
+
+   editPlayer(newPlayerData:any){
+     const $key = newPlayerData.$key;
+     delete(newPlayerData.$key);
+     this.db.list('/players').update($key, newPlayerData);
+   }
+
+
 }
